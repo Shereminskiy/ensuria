0dac0ed1f4f71382be01f07cf35baa3a8d900ca3
Oleksandr Shereminskyi <aalex.prog@gmail.com>
2024-07-20 00:37:49 +0300

    fix(code): Reformatted code style

--- database/repositories/Payment.js
+++ database/repositories/Payment.js
@@ -1,88 +1,89 @@
-import BaseRepository from "./Base.js";
+import BaseRepository from './Base.js';
 import Payments from '../models/Payments.js';
 import Shops from '../models/Shops.js';
-import {PAYMENT_STATE} from "../../constants.js";
+import { PAYMENT_STATE } from '../../constants.js';
 
-class PaymentRepository extends BaseRepository
-{
-    /**
-     * @param {Object<*>} item
-     * @returns {Promise<Payments>}
-     */
-    async findBy(item) {
-        return Payments.query().findOne(item);
-    }
+class PaymentRepository extends BaseRepository {
+  /**
+   * @param {Object<*>} item
+   * @returns {Promise<Payments>}
+   */
+  async findBy(item) {
+    return Payments.query().findOne(item);
+  }
 
-    /**
-     * @param {Object<*>} data
-     * @returns {Promise<Payments>}
-     */
-    async create(data = {}) {
-        return Payments.query().insertAndFetch(data);
-    }
+  /**
+   * @param {Object<*>} data
+   * @returns {Promise<Payments>}
+   */
+  async create(data = {}) {
+    return Payments.query().insertAndFetch(data);
+  }
 
-    /**
-     *
-     * @param {Array<number>} ids
-     * @param {Object<*>} where
-     * @returns {Objection.QueryBuilder<Payments, Payments[]>}
-     */
-    async findAllByIds(ids = [], where = {} ) {
-        return Payments.query().withGraphFetched('shop').whereIn('id', ids).andWhere(where);
-    }
+  /**
+   *
+   * @param {Array<number>} ids
+   * @param {Object<*>} where
+   * @returns {Objection.QueryBuilder<Payments, Payments[]>}
+   */
+  async findAllByIds(ids = [], where = {}) {
+    return Payments.query().withGraphFetched('shop').whereIn('id', ids).andWhere(where);
+  }
 
-    /**
-     *
-     * @param {Object<*>} where
-     * @returns {Objection.QueryBuilder<Payments, Payments[]>}
-     */
-    async findAllBy(where = {}) {
-        return Payments.query().withGraphFetched('shop').where(where).debug();
-    }
+  /**
+   *
+   * @param {Object<*>} where
+   * @returns {Objection.QueryBuilder<Payments, Payments[]>}
+   */
+  async findAllBy(where = {}) {
+    return Payments.query().withGraphFetched('shop').where(where);
+  }
 
-    /**
-     *
-     * @param {number} shop_id
-     * @param {string} interval
-     * @returns {Objection.QueryBuilder<Payments, Payments[]>}
-     */
-    async findAllReadyToPayByShopId(shop_id, interval = '1 day') {
-        return Payments.query()
-        .withGraphJoined('shop')
-        .whereIn('state', [PAYMENT_STATE.PROCESSED, PAYMENT_STATE.COMPLETED])
-        .andWhere('shop.id', shop_id)
-        .andWhereRaw(`now() > shop.payout_at + interval '${interval}'`);
-    }
+  /**
+   *
+   * @param {number} shop_id
+   * @param {string} interval
+   * @returns {Objection.QueryBuilder<Payments, Payments[]>}
+   */
+  async findAllReadyToPayByShopId(shop_id, interval = '1 day') {
+    return Payments.query()
+      .withGraphJoined('shop')
+      .whereIn('state', [PAYMENT_STATE.PROCESSED, PAYMENT_STATE.COMPLETED])
+      .andWhere('shop.id', shop_id)
+      .andWhereRaw(`now() > shop.payout_at + interval '${interval}'`);
+  }
 
-    /**
-     *
-     * @param {integer} shop_id
-     * @param {Array<Payments>} payments
-     * @returns {Promise<Objection.Transaction>}
-     */
-    async performPayment(shop_id, payments = []){
-        return Payments.transaction(async trx => {
-            let payout = 0;
+  /**
+   *
+   * @param {integer} shop_id
+   * @param {Array<Payments>} payments
+   * @returns {Promise<Objection.Transaction>}
+   */
+  async performPayment(shop_id, payments = []) {
+    return Payments.transaction(async (trx) => {
+      let payout = 0;
 
-            const promises = [];
-            for (const payment of payments) {
-                promises.push(payment.$query(trx).update({
-                    state: PAYMENT_STATE.PAID,
-                    updated_at: new Date().toISOString(),
-                }));
+      const promises = [];
+      for (const payment of payments) {
+        promises.push(
+          payment.$query(trx).update({
+            state: PAYMENT_STATE.PAID,
+            updated_at: new Date().toISOString(),
+          })
+        );
 
-                payout += payment.payout;
-            }
+        payout += payment.payout;
+      }
 
-            await Promise.all(promises);
-            const shop = await Shops.query(trx).findOne({id: shop_id}).debug()
+      await Promise.all(promises);
+      const shop = await Shops.query(trx).findOne({ id: shop_id });
 
-            await shop.$query(trx).update({
-                balance: shop.balance += payout,
-                payout_at: new Date().toISOString()
-            });
-        });
-    }
+      await shop.$query(trx).update({
+        balance: (shop.balance += payout),
+        payout_at: new Date().toISOString(),
+      });
+    });
+  }
 }
 
 export default new PaymentRepository();
--- database/repositories/Settings.js
+++ database/repositories/Settings.js
@@ -1,38 +1,37 @@
 import _ from 'lodash';
-import BaseRepository from "./Base.js";
+import BaseRepository from './Base.js';
 import Settings from '../models/Settings.js';
 import { SETTINGS } from '../../constants.js';
 
-class SettingsRepository extends BaseRepository
-{
-    /**
-     * @param {Object<*>} item
-     * @returns {Promise<Settings>}
-     */
-    async findBy(item) {
-        return Settings.query().findOne(item);
-    }
+class SettingsRepository extends BaseRepository {
+  /**
+   * @param {Object<*>} item
+   * @returns {Promise<Settings>}
+   */
+  async findBy(item) {
+    return Settings.query().findOne(item);
+  }
 
-    /**
-     * @param {Object<*>} data
-     * @returns {Promise<Settings>}
-     */
-    async create(data) {
-        return Settings.query().insertAndFetch(data);
-    }
+  /**
+   * @param {Object<*>} data
+   * @returns {Promise<Settings>}
+   */
+  async create(data) {
+    return Settings.query().insertAndFetch(data);
+  }
 
-    /**
-     * @returns {Promise<Dictionary<Settings>>}
-     */
-    async getPlatformCommission() {
-        const settings = await Settings.query().whereIn('name', [
-            SETTINGS.COMMISSION_A,
-            SETTINGS.COMMISSION_B,
-            SETTINGS.COMMISSION_D
-        ]);
+  /**
+   * @returns {Promise<Dictionary<Settings>>}
+   */
+  async getPlatformCommission() {
+    const settings = await Settings.query().whereIn('name', [
+      SETTINGS.COMMISSION_A,
+      SETTINGS.COMMISSION_B,
+      SETTINGS.COMMISSION_D,
+    ]);
 
-        return _.keyBy(settings, 'name');
-    }
+    return _.keyBy(settings, 'name');
+  }
 }
 
 export default new SettingsRepository();
